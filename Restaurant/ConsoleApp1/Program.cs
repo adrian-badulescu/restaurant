4a690340856bfa2568ef258a9fa043416497cdd4
adrian-badulescu@outlook.com <adrian-badulescu@outlook.com>
2019-09-28 17:13:43 +0300

    dialog for removin tables added

--- Restaurant/ConsoleApp1/Program.cs
+++ Restaurant/ConsoleApp1/Program.cs
@@ -11,24 +11,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Room section
 
-            Console.WriteLine("Create a new room, plese enter the numeric id: ");
-            var r = new Room(Convert.ToInt32(Console.ReadLine()));
 
 
             bool confirmed = false;
             //Table[] TablesArray = new Table[100];
             Table t;
-            List<Table> TablesLst = new List<Table>(); // could 
+            List<Table> TablesLst = new List<Table>();
+
+
+
+            // Room section
+
+            Console.WriteLine("Create a new room, plese enter the numeric id: ");
+            var r = new Room(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine();
+
             do
             {
                 Console.Write("Enter the taken table Id (string): ");
 
                 t = new Table(Console.ReadLine());
-
+                Console.WriteLine();
+                Console.WriteLine();
                 Console.Write("Add another table y? [y/n] ");
-                Console.WriteLine("You entered, " + t.tableId + " as your table id !");
+                Console.WriteLine();
+                Console.WriteLine("You have entered, " + t.tableId + " as your table id !");
 
                 ConsoleKey response;
                 do
@@ -46,71 +55,119 @@ namespace ConsoleApp1
 
             } while (!confirmed);
 
-            Console.WriteLine("These are the created tables");
+            Console.WriteLine("These are the created tables: ");
 
             foreach (var table in TablesLst)
-            {
-               
+            {               
                 Console.WriteLine("############################");
                 Console.WriteLine("Table: " + table.tableId);
                 Console.WriteLine("############################");
             }
 
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            
+
+            do
+            {
+
+                Console.Write("Do you want to delete any of these tables y? [y/n]: ");
+                Console.ReadLine();
+                Console.Write("Which one ? (select index): ");
+                TablesLst.RemoveAt(Convert.ToInt32(Console.ReadLine()));
+                //Console.ReadLine();                
+                Console.Write("Do you want to delete another table y? [y/n]: ");
+                
+
+                ConsoleKey response;
+                do
+                {
+
+                    response = Console.ReadKey(false).Key;   // true is intercept key (dont show), false is show
+                    if (response != ConsoleKey.Enter)
+                        Console.WriteLine();
+
+                } while (response != ConsoleKey.Y && response != ConsoleKey.N);
 
 
 
 
+                confirmed = response == ConsoleKey.N;
+                
 
-            //Table section
+
+            } while (!confirmed);
+
+
+
+
+            Console.WriteLine("These are the remained tables: ");
+
+
+
+            foreach (var table in TablesLst)
+            {
+                Console.WriteLine("############################");
+                Console.WriteLine("Table: " + table.tableId);
+                Console.WriteLine("############################");
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Table section;
 
             //Console.Write("Enter the taken table Id: ");
             //var t = new Table(Convert.ToInt32(Console.ReadLine()));
-            Console.Write("Enter how many seats have the table: ");
-            t.totalSeatsOfTable = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter how many persons: ");
-            t.takenSeats = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("###############################################");
-            Console.WriteLine("         % ocupare " + t.Tableocupancy());
-            Console.WriteLine("###############################################");
-            Console.WriteLine("");
-            Console.WriteLine("The table state is: " + t.TableState());
+            //
+
+            //Console.Write("Enter how many seats have the table: ");
+            //t.totalSeatsOfTable = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter how many persons: ");
+            //t.takenSeats = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("############################");
+            //Console.WriteLine(" % ocupare: " + t.Tableocupancy());
+            //Console.WriteLine("############################");
+            //Console.WriteLine("");
+            //Console.WriteLine("The table state is: " + t.TableState());
 
 
 
 
 
 
-            //r.tables.Add(t);
+            ////r.tables.Add(t);
 
-            foreach (var ts in r.TablesState())
-            {
+            //foreach (var ts in r.TablesState())
+            //{
 
-                //                Console.WriteLine(ts.Key +","+ts.Value);
-                Console.WriteLine($"idtable={ts.Key} state={ts.Value}");
-
-
-            }
+            //    //                Console.WriteLine(ts.Key +","+ts.Value);
+            //    Console.WriteLine($"idtable={ts.Key} state={ts.Value}");
 
 
-            // Waiter section
-
-            // 2nd arg of Waiter should be "P" - available ; "A" - absent from work
-            Console.WriteLine("Please enter waiter's name and press Enter \nfollowed by 'P' if is working today or 'A' if not an press Enter : ");
-            var w = new Waiter(Console.ReadLine(), Console.ReadLine());
-            Console.WriteLine("Please enter the total number of waiters: ");
-            w.totalWaitersOfRestaurant = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(w.WaiterState());        
-
-            //Console.WriteLine("% presence" + w.presenceAtWork());
-            w.waiters.Add(w);
+            //}
 
 
-            foreach (var ws in w.WaitersState())
-            {
-                Console.WriteLine($"idWaiter={ws.Key} state={ws.Value}");
-            }
+            //// Waiter section
+
+            //// 2nd arg of Waiter should be "P" - available ; "A" - absent from work
+            //Console.WriteLine("Please enter waiter's name and press Enter \nfollowed by 'P' if is working today or 'A' if not an press Enter : ");
+            //var w = new Waiter(Console.ReadLine(), Console.ReadLine());
+            //Console.WriteLine("Please enter the total number of waiters: ");
+            //w.totalWaitersOfRestaurant = Convert.ToInt32(Console.ReadLine());
+            ////Console.WriteLine(w.WaiterState());        
+
+            ////Console.WriteLine("% presence" + w.presenceAtWork());
+            //w.waiters.Add(w);
+
+
+            //foreach (var ws in w.WaitersState())
+            //{
+            //    Console.WriteLine($"idWaiter={ws.Key} state={ws.Value}");
+            //}
 
 
 
